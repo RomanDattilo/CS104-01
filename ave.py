ba4c44240fbee1985f92b535b8e501bc527294ee
RomanDattilo <114018508+RomanDattilo@users.noreply.github.com>
2022-10-03 21:16:44 -0400

    Add files via upload

---
+++ ave.py
@@ -0,0 +1,8 @@
+average=0
+total=0
+howManyEntered=0
+howMany = int(input("How many test scores would you like to average?:" ))
+testScore=float(input("Enter teest score"))
+testScore=float(input("Enter teest score"))
+testScore=float(input("Enter teest score"))
+total = total + testScore
