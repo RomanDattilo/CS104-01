13490e5f100a6710e657358746a0d1a221931551
RomanDattilo <114018508+RomanDattilo@users.noreply.github.com>
2022-10-11 13:32:58 -0400

    Add files via upload

--- conditions.py
+++ conditions.py
@@ -1,22 +1,14 @@
-Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> temp=int(input("Please enter the current temperature: "))
-Please enter the current temperature: 57
->>> x=90
->>> y=temp
->>> z=70
->>> a=50
->>> b=32
->>> if y>x:
-...     print("Wear Shorts")
-... elif y>z:
-...     print("Short sleeves are fine")
-... elif y>a:
-...     print("Wear a jacket")
-... elif y>b:
-...     print("Wear a heavy coat")
-... else:
-...     print("Stay Inside")
-... 
-...     
-Wear a jacket
+temp=int(input("Please enter the current temperature: "))
+x=90
+y=temp
+z=70
+a=50
+b=32
+if y>x: print("Wear Shorts")
+elif y>z: print("Short sleeves are fine")
+elif y>a: print("Wear a jacket")
+elif y>b: print("Wear a heavy coat")
+else: print("Stay Inside")
+     
+
+
